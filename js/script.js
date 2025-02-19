bc537996c8225f6a378866b891a3aa1aa8962780
Dramatopian <flyx006@gmail.com>
2025-02-19 11:14:42 +0800

    Edit files

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const socket = new WebSocket("wss://api.lanyard.rest/socket");
+
+socket.onopen = () => {
+  socket.send(
+    JSON.stringify({
+      op: 2,
+      d: {
+        subscribe_to_id: "620229764996923402",
+      },
+    })
+  );
+};
+
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  console.log(data);
+};
+
+const DISCORD_ID = "620229764996923402";
+
+async function fetchLanyard() {
+    try {
+        const response = await fetch(`https://api.lanyard.rest/v1/users/${DISCORD_ID}`);
+        const { data } = await response.json();
+
+        let activityText = [];
+        let isListeningToSpotify = false;
+
+        if (data.spotify) {
+            isListeningToSpotify = true;
+            activityText.push(`Listening to Spotify - ${data.spotify.song}`);
+        }
+
+        data.activities.forEach(activity => {
+            if (isListeningToSpotify && activity.name === "Spotify") {
+                return;
+            }
+
+            if (activity.type === 0) { 
+                activityText.push(`Playing ${activity.name}`);
+            } else {
+                activityText.push(`On ${activity.name}`);
+            }
+        });
+
+        const activityContainer = document.getElementById("current-activity");
+        activityContainer.innerHTML = activityText.length > 0 
+        ? `Currently:<br><span>${activityText.join("<br>")}</span>` 
+        : "No current activity";
+
+    } catch (error) {
+        console.error("Error fetching Lanyard data:", error);
+    }
+}
+
+fetchLanyard();
+setInterval(fetchLanyard, 5000);
+
